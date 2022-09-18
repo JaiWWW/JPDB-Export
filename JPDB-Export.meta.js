3c540c58b072bec8a5f4b0cf894c9296a72151f8
Jai <108700712+JaiWWW@users.noreply.github.com>
2022-09-18 20:12:46 +0100

    Create JPDB-Export.meta.js

---
+++ JPDB-Export.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         JPDB-Export
+// @namespace    http://tampermonkey.net/
+// @version      1.1.6
+// @description  Allows you to export your JPDB decks (see readme on github for more info)
+// @author       JaiWWW
+// @license      GPL-3.0
+// @match        https://jpdb.io/deck?*
+// @match        https://jpdb.io/add-to-deck-from-shirabe-jisho*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jpdb.io
+// @homepageURL  https://github.com/JaiWWW/JPDB-Export
+// @supportURL   https://github.com/JaiWWW/JPDB-Export/issues/new
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
